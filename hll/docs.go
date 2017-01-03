4ffae12f40cdaebd40047629dbf13deb7a1b3dc4
Chavez <matthew@el-chavez.me>
2017-01-03 12:56:09 -0800

    Go styling
    - go vet fix
    - golint fix

--- hll/docs.go
+++ hll/docs.go
@@ -1,4 +1,4 @@
-// package hll implements the hyper-log-log sketch algorithm to track unique
+// Package hll implements the hyper-log-log sketch algorithm to track unique
 // counts/occurrences of items within some pre-defined error rate where there
 // are generally no false negatives
 package hll

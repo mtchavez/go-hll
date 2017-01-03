6a42d98e3fa9db8b32d86e845c951757be9adbbe
Chavez <matthew@el-chavez.me>
2017-01-03 12:52:25 -0800

    Add a package docs.go file

---
+++ hll/docs.go
@@ -0,0 +1,4 @@
+// package hll implements the hyper-log-log sketch algorithm to track unique
+// counts/occurrences of items within some pre-defined error rate where there
+// are generally no false negatives
+package hll
